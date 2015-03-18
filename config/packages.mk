cb4ee3535d0fbac4fb3246818a4ed0c685b35b24
theringmaster <gzroms@hotmail.com>
2015-03-18 19:12:45 -0400

    Add supersu

--- config/packages.mk
+++ config/packages.mk
@@ -2,3 +2,9 @@
 PRODUCT_PACKAGES += \
     LockClock
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/tesla/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/tesla/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+
